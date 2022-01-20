1d60fbe715ad1a1dc98fba9b991d9ad8e9ce588c
Brandon Hunsaker <brandondwight42@gmail.com>
2022-01-20 15:23:19 -0700

    Problem 10 working

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -139,9 +139,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-for(i = letters.length - 1; i >= 0; i--){
-  console.log(letters[i])
-}
+// for(i = letters.length - 1; i >= 0; i--){
+//   console.log(letters[i])
+// }
 
 ////////// Advanced Problems //////////
 
@@ -163,7 +163,28 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+switch(letterGrade){
+  case 'A':
+  console.log("The student is doing excellently.")
+  break
+  case 'B':
+  console.log("The student is doing well.")
+  break
+  case 'C':
+  console.log("The student is doing alright.")
+  break
+  case 'D':
+  console.log("The student is not doing very well.")
+  break
+  case 'F':
+  console.log("The studnet is failing.")
+  break
+  default:
+  console.log("Not an eligible grade.")
 
+
+
+}
 
 
 ////////// PROBLEM 11 //////////
